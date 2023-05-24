ff7f6d9600458f6440447aba5f2d6e91e7c32a59
Winston <winstonleung@Winstons-MBP.ht.home>
2023-05-23 22:16:09 -0400

    Working db connectino

--- db.js
+++ db.js
@@ -4,9 +4,10 @@ const mysql = require("mysql")
 let db_conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ''
+    password: 'cs348cs348'
 });
 
+// Movie (title, rating, release)
 
 db_conn.connect((err) => {
     if (err) {
@@ -16,9 +17,31 @@ db_conn.connect((err) => {
     }
 });
 
-let db_name = 'helloworld'
+let db_name = 'projectdb'
 let create_query = `CREATE DATABASE IF NOT EXISTS ${db_name}`;
 
+db_conn.query(create_query)
+
+let useQuery = `USE ${db_name}`;
+db_conn.query(useQuery);
+
+let create_table_query = 'CREATE TABLE IF NOT EXISTS Movie(title VARCHAR(100) NOT NULL, rating DOUBLE(2,1) NOT NULL, release_date DATE NOT NULL);'
+db_conn.query(create_table_query, (error) => {
+  if(error) throw error;
+  console.log("Created Table");
+});
+
+db_conn.query("INSERT INTO Movie VALUES('ABC', 7.8, '2023-03-04');")
+db_conn.query("INSERT INTO Movie VALUES('DEF', 8.1, '2022-01-15');")
+db_conn.query("INSERT INTO Movie VALUES('XYZ', 9.9, '2020-06-30');")
+
+db_conn.query("SELECT * FROM Movie;", (error, results, fields) => {
+  console.log(results);
+})
+
+db_conn.end()
+  
+/*
 db_conn.query(create_query, (err) => {
     if(err) throw err;
 
@@ -31,17 +54,18 @@ db_conn.query(create_query, (err) => {
         console.log("Using Database");
     });
 
-    let create_table_query = 'CREATE TABLE IF NOT EXISTS CALENDAR(MONTHS DATE NOT NULL);'
+    let create_table_query = 'CREATE TABLE IF NOT EXISTS Movie(title VARCHAR(100) NOT NULL, rating DOUBLE(2,1) NOT NULL, release_date DATE NOT NULL);'
     db_conn.query(create_table_query, (error) => {
       if(error) throw error;
       console.log("Created Table");
     });
 
-    db_conn.query("INSERT INTO CALENDAR(MONTHS) VALUES('2023-01-01');")
-    db_conn.query("INSERT INTO CALENDAR(MONTHS) VALUES('2023-02-01');")
-    db_conn.query("INSERT INTO CALENDAR(MONTHS) VALUES('2023-03-01');")
+    db_conn.query("INSERT INTO Movie VALUES('ABC', 7.8, '2023-03-04');")
+    db_conn.query("INSERT INTO Movie VALUES('DEF', 8.1, '2022-01-15');")
+    db_conn.query("INSERT INTO Movie VALUES('XYZ', 9.9, '2020-06-30');")
     
-    console.log(db_conn.query("SELECT * FROM CALENDAR;"))
+    // console.log(db_conn.query("SELECT * FROM Movie;"))
 });
   
 module.exports = db_conn;
+*/
