13a05933fb70fb3dd44f46533ede1941d23812d8
JerryW8 <jerry.wei08@gmail.com>
2023-06-21 17:00:20 -0400

    Fixed sql indentation

--- test-sample.sql
+++ test-sample.sql
@@ -1,14 +1,14 @@
 -- Gets the crew members who produced the media and their other films
 -- NOTE: "tgf" is just a placeholder
 SELECT * FROM Crew
-	NATURAL JOIN Produced
+  NATURAL JOIN Produced
   NATURAL JOIN Media
   WHERE crewID IN (SELECT crewID FROM Produced WHERE titleID = "tgf")
 
 -- Gets the actors who played in the media and their other films
 -- NOTE: "tgf" is just a placeholder
 SELECT * FROM Role
-	NATURAL JOIN PlaysIn
+  NATURAL JOIN PlaysIn
   NATURAL JOIN Media
   WHERE roleID IN (SELECT roleID FROM PlaysIn WHERE titleID = "tgf")
 
