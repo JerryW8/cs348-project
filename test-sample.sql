8067a092ff69042100b3b00ff3473047e5ca5341
JerryW8 <jerry.wei08@gmail.com>
2023-07-11 03:34:47 -0400

    Added ;

--- test-sample.sql
+++ test-sample.sql
@@ -1,23 +1,23 @@
 -- Gets the media with given title
 -- NOTE: "Titanic" is just a place holder
-SELECT * FROM media WHERE originalTitle LIKE "%Titanic%"
+SELECT * FROM media WHERE originalTitle LIKE "%Titanic%";
 
 -- Gets the media with given genre
 -- NOTE: "Drama" is just a place holder
-SELECT * FROM media WHERE genre = "Drama"
+SELECT * FROM media WHERE genre = "Drama";
 
 -- Gets the media with given rating
 -- NOTE: 8.0 is just a place holder
-SELECT * FROM media WHERE rating >= 8.0
+SELECT * FROM media WHERE rating >= 8.0;
 
 -- Gets the media with given startYear
 -- NOTE: 1997 is just a place holder
 SELECT * FROM media WHERE startYear >= 1997
-ORDER BY startYear DESC
+ORDER BY startYear DESC;
 
 -- Gets the recommended media with given genre and given titileID
 -- NOTE: "Romance" and "ttc" are just place holders
-SELECT * FROM media WHERE genre = "Romance" AND rating >= 8.5 AND titleID <> "ttc"
+SELECT * FROM media WHERE genre = "Romance" AND rating >= 8.5 AND titleID <> "ttc";
 
 -- Gets the crew members who produced the media and their other films
 -- NOTE: "tgf" is just a placeholder
@@ -26,7 +26,7 @@ SELECT * FROM Crew
  	NATURAL JOIN Media
  	WHERE RIGHT(CrewID, 9) IN 
   (SELECT RIGHT(crewID, 9) FROM PRODUCED 
-  WHERE titleID = "tgf") AND titleID <> "tgf"
+  WHERE titleID = "tgf") AND titleID <> "tgf";
 
 -- Gets the actors who played in the media and their other films
 -- NOTE: "tgf" is just a placeholder
@@ -35,7 +35,7 @@ SELECT * FROM Role
  	NATURAL JOIN Media
  	WHERE RIGHT(roleID, 9) IN 
   (SELECT RIGHT(roleID, 9) FROM PlaysIn 
-  WHERE titleID = "tgf") AND titleID <> "tgf"
+  WHERE titleID = "tgf") AND titleID <> "tgf";
 
 -- Add a movie to watchlist
 INSERT INTO Collection(collectionID) VALUES(NULL);
